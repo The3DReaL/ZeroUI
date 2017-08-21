74eda79f0bf8c43a263a5026190b3bfc6144941e
The3DReaL <The3DReaL@gmail.com>
2017-08-21 20:45:21 +0300

    Creating meth_helping_in_time_gui.lua
    Based on time_gui.jpg

---
+++ ZeroUI/meth_helping_in_time_gui.lua
@@ -0,0 +1,16 @@
+--[["hook_id": "lib/managers/dialogmanager"
+Code for time_gui.jpg
+]]
+
+if ZeroUI_settings.meth_helping_on_time_gui then --Setting for this code if needed
+--[[
+local meth_target_dialog = {
+	pln_rt1_20 = "Add Muriatic Acid",
+	pln_rt1_22 = "Add Caustic Soda",
+	pln_rt1_24 = "Add Hydrogen Chloride", -- text from time_gui.jpg
+	pln_rat_stage1_20 = "Add Muriatic Acid",
+	pln_rat_stage1_22 = "Add Caustic Soda",
+	pln_rat_stage1_24 = "Add Hydrogen Chloride", -- text from time_gui.jpg
+-- 28 not need, because it is target
+}
+]]
